817165068578adf28fc23a9f8e4446f4597b4e40
Emerick Bosch <xpc.dev@gmail.com>
2019-09-22 19:29:20 +0200

    Use npm; fix tsconfig and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  transform: {
-    "^.+\\.ts?$": "ts-jest"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$",
-  moduleFileExtensions: ["ts", "js", "json", "node"]
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
